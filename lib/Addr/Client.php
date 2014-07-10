add4e38c1c6e4ccc95bbb9eda700db5e27a947e4
Mateusz Nowak <kontakt@estshy.pl>
2014-07-10 18:00:46 +0200

    Prevent from crashes when url doesn't exists
    Cherry picked from 3655b79..0d62434

--- lib/Addr/Client.php
+++ lib/Addr/Client.php
@@ -255,7 +255,10 @@ class Client
      */
     private function completePendingLookup($id, $addr, $type)
     {
-        call_user_func($this->pendingLookups[$id]['callback'], $addr, $type);
+        if (isset($this->pendingLookups[$id])) {
+            call_user_func($this->pendingLookups[$id]['callback'], $addr, $type);
+        }
+
         unset($this->pendingLookups[$id]);
     }
 
