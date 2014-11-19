ebce062f634d883410c5b5353421662e324ff238
Danack <Danack@basereality.com>
2014-11-19 19:03:31 +0000

    Added check to stop myself being confused by the tests not running.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,6 +4,12 @@ use Auryn\Provider;
 
 error_reporting(E_ALL);
 
+if (ini_get('opcache.enable') == true &&
+    ini_get('opcache.save_comments') == false) {
+    echo "Cannot run tests. OPCache is enabled and is stripping comments, which are required by PHPUnit to provide data for the tests.\n";
+    exit(-1);
+}
+
 function createProvider($implementations = [], $shareClasses = []) {
     $provider = new Provider();
 
