4ef49e85441e21377c6fca4316bb52dcea7a1afb
Niklas Keller <me@kelunik.com>
2017-06-14 08:08:34 +0200

    Fix cache TTL

--- lib/DefaultResolver.php
+++ lib/DefaultResolver.php
@@ -667,9 +667,9 @@ REGEX;
             $promisor->fail($error);
         } else {
             foreach ($result as $type => $records) {
-                $minttl = INF;
+                $minttl = \PHP_INT_MAX;
                 foreach ($records as list( , , $ttl)) {
-                    if ($ttl && $minttl > $ttl) {
+                    if ($ttl < $minttl) {
                         $minttl = $ttl;
                     }
                 }
