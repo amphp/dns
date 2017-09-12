b33ec8043fee6fc500de3ec220953ae63a093afd
Niklas Keller <me@kelunik.com>
2017-09-12 09:38:26 +0200

    Add example for a custom config

---
+++ examples/custom-config.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use Amp\Dns;
+use Amp\Loop;
+use Amp\Promise;
+
+$customConfigLoader = new class implements Dns\ConfigLoader {
+    public function loadConfig(): Promise {
+        return Amp\call(function () {
+            $hosts = yield (new Dns\HostLoader)->loadHosts();
+
+            return new Dns\Config([
+                "8.8.8.8:53",
+                "[2001:4860:4860::8888]:53",
+            ], $hosts, $timeout = 5000, $attempts = 3);
+        });
+	}
+};
+
+Dns\resolver(new Dns\BasicResolver(null, $customConfigLoader));
+
+Loop::run(function () {
+    var_dump(yield Dns\resolve("google.com"));
+});
