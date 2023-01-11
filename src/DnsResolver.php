7344cceb95d6e66bf8c867e0b50417ced4b055d8
Aaron Piotrowski <aaron@trowski.com>
2023-01-11 17:50:18 -0600

    More specific DnsResolver return types

--- src/DnsResolver.php
+++ src/DnsResolver.php
@@ -9,29 +9,31 @@ interface DnsResolver
     /**
      * Resolves a hostname name to an IP address [hostname as defined by RFC 3986].
      *
-     * Upon success the returned promise resolves to an array of Record objects.
+     * Upon success this method returns an array of Record objects. If the domain cannot be resolved,
+     * a {@see DnsException} is thrown.
      *
      * A null $ttl value indicates the DNS name was resolved from the cache or the local hosts file.
      *
      * @param string $name The hostname to resolve.
      * @param int|null $typeRestriction Optional type restriction to `Record::A` or `Record::AAAA`, otherwise `null`.
      *
-     * @return array<DnsRecord>
+     * @return non-empty-list<DnsRecord>
      *
      * @throws MissingDnsRecordException
      * @throws DnsException
      */
-    public function resolve(string $name, int $typeRestriction = null, ?Cancellation $cancellation = null): array;
+    public function resolve(string $name, ?int $typeRestriction = null, ?Cancellation $cancellation = null): array;
 
     /**
      * Query specific DNS records.
      *
-     * Upon success the returned promise resolves to an array of Record objects.
+     * Upon success this method returns an array of Record objects. If no records of the given type are found,
+     * a {@see DnsException} is thrown.
      *
      * @param string $name Record to question, A, AAAA and PTR queries are automatically normalized.
      * @param int $type Use constants of Amp\Dns\Record.
      *
-     * @return array<DnsRecord>
+     * @return non-empty-list<DnsRecord>
      *
      * @throws DnsException
      */
